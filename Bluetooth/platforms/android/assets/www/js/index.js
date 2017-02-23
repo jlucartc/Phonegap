eeac57fb5a0154aa8e7097cbd4c7e36f56cfe525
Luca <Luca>
2017-02-23 16:08:40 -0300

    Commit Bluetooth

--- Bluetooth/platforms/android/assets/www/js/index.js
+++ Bluetooth/platforms/android/assets/www/js/index.js
@@ -48,7 +48,15 @@
     }
 };*/
 
-document.addEventListener('deviceready',function(){startScan();},function(){alert("Fail!");});
+
+document.addEventListener('deviceready',function(){enable();},function(){alert("Fail!");});
+
+function enable(){
+  var escanear = document.getElementById('escanear');
+  var parar = document.getElementById('parar');
+  escanear.disabled = false;
+  parar.disabled = false;
+}
 
 var scanning = false;
 var beaconsIds = ['F7:5D:EA:B9:57:58','F2:09:F5:7D:E9:AB'];
@@ -68,58 +76,36 @@ function startScan(){
   if(localStorage.exit == 'true'){
     navigator.app.exitApp();
   }
-  ble.isEnabled(function(){/*alert("Enabled.");*/
+  var title = document.getElementById('title');
+  title.innerHTML = "Escaneando...";
+  var escanear = document.getElementById('escanear');
+  var parar = document.getElementById('parar');
+  escanear.className = 'btn btn-primary';
+  parar.className = 'btn btn-default';
+  ble.isEnabled(function(){
       localStorage.exit = false;
       if(scanning == false){
       clean();
-      /*var escanear = document.getElementById('escanear');
-      var parar = document.getElementById('parar');
-      escanear.className = 'btn btn-primary';
-      parar.className = 'btn btn-default';*/
-      //alert("Escaneando...");
       scanning = true;
       ble.startScanWithOptions([],{reportDuplicates : true},function(device){
         for(var i = 0; i < beaconsIds.length; i++){
           if(device.id == beaconsIds[i] && device.rssi >= -70){
-            /*ble.connect(device.id,function(peripheral){
-                /*ble.read(device.id,"b9404000-f5f8-466e-aff9-25556b57fe6d","b9404002-f5f8-466e-aff9-25556b57fe6d",function(data){
-                //var power = new Uint8Array(data);
-                if((power[0]*-1) <= device.rssi){
-                  for (var i = 0; i < beaconsIds.length; i++) {
-                    if(beaconsIds[i] == device.id){
-                      showMessage(i);
-                    }
-                  }
-                }
-              },function(err){alert(err);});
-            },function(){
-            ble.disconnect(device.id,function(){},function(){});
-          });*/
             showMessage(i);
           }
         }
-        /*var newButton = document.createElement('button');
-        var name = document.createTextNode(device.name+" - "+device.id+" - "+device.rssi);
-        newButton.value = device.id;
-        newButton.onclick = function(){ devPage(device.id); }
-        newButton.className = 'list-group-item';
-        newButton.appendChild(name);
-        var lista = document.getElementById('dev-list').appendChild(newButton);*/
       },function(){alert("Erro ao iniciar o scan!");});
     }},function(){ble.enable(startScan,false);});
-  //ble.enable(function(){this.startScan();},false);
-  //ble.startStateNotifications(function(state){if(state != 'on'){ble.stopStateNotifications();ble.enable(function(){alert("Bluetooth enabled");startScan();},false);}else{ble.stopStateNotifications();alert('Bluetooth is '+state);}},false);
-
 }
 
 function stopScan(){
+  var title = document.getElementById("title");
+  title.innerHTML = "";
   if(scanning == true){
     var escanear = document.getElementById('escanear');
     var parar = document.getElementById('parar');
     escanear.className = 'btn btn-default';
     parar.className = 'btn btn-primary';
     ble.stopScan(function(){
-      alert("Parando o escaneamento!");
       scanning = false;
     },function(){
       alert("Erro ao parar o escaneamento!");
